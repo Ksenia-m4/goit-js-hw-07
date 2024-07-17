691573e3f18f94c0f881d3e7a3205876228d5185
Kseniia Machnieva <164485727+Ksenia-m4@users.noreply.github.com>
2024-07-17 11:10:43 +0200

    creating the repository

---
+++ js/01-gallery.js
@@ -0,0 +1,6 @@
+import { galleryItems } from "./gallery-items.js";
+// В файле gallery-items.js есть массив galleryItems, который содержит объекты с информацией о изображениях: маленькое (превью), оригинальное (большое) и описание. Мы уже подключили его к каждому из JS-файлов проекта.
+
+// Change code below this line
+
+console.log(galleryItems);
